127fa041143e50ff6b38f06a7c404ffc50ad94a0
supple <marekmazu@gmail.com>
2017-01-01 10:26:55 +0100

    Ci on clicleci

--- tests/helper.go
+++ tests/helper.go
@@ -7,7 +7,7 @@ import (
 var TEST_CUSTOMER = "ctest"
 
 func GetTestStorage() {
-    db := storage.NewMongoDB("192.168.1.106:27017", "unittest")
+    db := storage.NewMongoDB("127.0.0.1:27017", "unittest")
     storage.SetInstance("crm", db)
     storage.DropDatabase(db)
 }
