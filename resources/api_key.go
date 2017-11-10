8967494dfc08fe0293c9e3338d0ec41f77ee1087
supple <marekmazu@gmail.com>
2017-11-10 14:05:50 +0100

    Set basic fields in models

--- resources/api_key.go
+++ resources/api_key.go
@@ -1,12 +1,12 @@
 package resources
 
 import (
-    "gopkg.in/mgo.v2"
-    "gopkg.in/mgo.v2/bson"
-    "github.com/supple/gorest/core"
-    "github.com/supple/gorest/model"
-    "github.com/supple/gorest/storage"
-    "fmt"
+	"fmt"
+	"github.com/supple/gorest/core"
+	"github.com/supple/gorest/model"
+	"github.com/supple/gorest/storage"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 const API_KEY_FIELD string = "apiKey"
@@ -15,125 +15,125 @@ const API_KEY_REPO = "crm"
 // ### -- ApiKey repo
 
 type ApiKeyRP struct {
-    gt *core.Gateway
-    cc *core.CustomerContext
-    db *storage.MongoDB
+	gt *core.Gateway
+	cc *core.CustomerContext
+	db *storage.MongoDB
 }
 
 func NewApiKeyRP(cc *core.CustomerContext) *ApiKeyRP {
-    rp := &ApiKeyRP{cc:cc}
-    db := storage.GetInstance(API_KEY_REPO)
-    d := core.EmptyDecorator()
-    gt := core.NewGateway(rp.CollectionName(), d, db)
+	rp := &ApiKeyRP{cc: cc}
+	db := storage.GetInstance(API_KEY_REPO)
+	d := core.EmptyDecorator()
+	gt := core.NewGateway(rp.CollectionName(), d, db)
 
-    rp.gt = gt
+	rp.gt = gt
 
-    return rp
+	return rp
 }
 
 func (rp *ApiKeyRP) Create(model *model.ApiKey) error {
-    var err error
-    model.CustomerName = rp.cc.CustomerName
+	var err error
+	model.CustomerName = rp.cc.CustomerName
 
-     // validate
-    customer, err := rp.ConstraintsValidation(model)
-    if (err != nil) {
-        return err
-    }
+	// validate
+	customer, err := rp.ConstraintsValidation(model)
+	if err != nil {
+		return err
+	}
 
-    fmt.Println("OK CUST is")
-    // create key if not set
-    if (len(model.ApiKey) == 0) {
-        model.ApiKey = fmt.Sprintf("%s-%s", core.RandString(24), customer.Hash)
-    }
-    err = rp.gt.Insert(model)
-    fmt.Println(model)
-    return err
+	// create key if not set
+	if len(model.ApiKey) == 0 {
+		model.ApiKey = fmt.Sprintf("%s-%s", core.RandString(32), customer.Hash)
+	}
+	model.SetBasicFields()
+
+	err = rp.gt.Insert(model)
+	fmt.Println(model)
+	return err
 }
 
 func (rp *ApiKeyRP) FindOne(id string) (*model.ApiKey, error) {
-    result := &model.ApiKey{}
-    err := rp.gt.FindById(id, result)
-    return result, err
+	result := &model.ApiKey{}
+	err := rp.gt.FindById(id, result)
+	return result, err
 }
 
 func (rp *ApiKeyRP) FindOneBy(conditions bson.M) (*model.ApiKey, error) {
-    result := &model.ApiKey{}
-    err := rp.gt.FindOneBy(conditions, result)
+	result := &model.ApiKey{}
+	err := rp.gt.FindOneBy(conditions, result)
 
-    return result, err
+	return result, err
 }
 
-func (rp *ApiKeyRP) Delete(id string) (error) {
-    err := rp.gt.Remove(id)
-    return err
+func (rp *ApiKeyRP) Delete(id string) error {
+	err := rp.gt.Remove(id)
+	return err
 }
 
 func (rp *ApiKeyRP) ConstraintsValidation(model *model.ApiKey) (*model.Customer, error) {
-    csRp := NewCustomerRP(rp.cc)
-    c, err := csRp.FindOneByName(model.CustomerName)
-    if (err == core.ErrNotFound) {
-        return nil, core.ValidationError{Field: "customerName", Message: "Customer not found: "+model.CustomerName}
-    }
-    if (err != nil) {
-        return nil, err
-    }
+	csRp := NewCustomerRP(rp.cc)
+	c, err := csRp.FindOneByName(model.CustomerName)
+	if err == core.ErrNotFound {
+		return nil, core.ValidationError{Field: "customerName", Message: "Customer not found: " + model.CustomerName}
+	}
+	if err != nil {
+		return nil, err
+	}
 
-    return c, err
+	return c, err
 }
 
 func (rp ApiKeyRP) CollectionName() string {
-    return "ApiKey"
+	return "ApiKey"
 }
 
 func Auth(apiKey string, accessTo AccessTo) (*core.CustomerContext, error) {
-    var cc *core.CustomerContext
+	var cc *core.CustomerContext
 
-    akRp := NewApiKeyRP(cc)
-    ak, err := akRp.FindOneBy(bson.M{API_KEY_FIELD: apiKey})
-    // @todo: hasAccess(accessTo)
-    if err == core.ErrNotFound {
-        return nil, core.ErrInvalidApiKey
-    }
+	akRp := NewApiKeyRP(cc)
+	ak, err := akRp.FindOneBy(bson.M{API_KEY_FIELD: apiKey})
+	// @todo: hasAccess(accessTo)
+	if err == core.ErrNotFound {
+		return nil, core.ErrInvalidApiKey
+	}
 
-    if (err != nil) {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    if (ak != nil) {
-        cc = &core.CustomerContext{}
-        // copy to customer context
-        cc.ApiKey = ak.ApiKey
-        cc.CustomerName = ak.CustomerName
-        cc.AppId = ak.AppId
-    }
+	if ak != nil {
+		cc = &core.CustomerContext{}
+		// copy to customer context
+		cc.ApiKey = ak.ApiKey
+		cc.CustomerName = ak.CustomerName
+		cc.AppId = ak.AppId
+	}
 
-    return cc, err
+	return cc, err
 }
 
 func CreateApiKey(cc *core.CustomerContext) (*model.ApiKey, error) {
-    akRp := NewApiKeyRP(cc)
-    ak := &model.ApiKey{}
-    ak.CustomerName = cc.CustomerName
-    err := akRp.Create(ak)
+	akRp := NewApiKeyRP(cc)
+	ak := &model.ApiKey{}
+	ak.CustomerName = cc.CustomerName
+	err := akRp.Create(ak)
 
-    return ak, err
+	return ak, err
 }
 
 func (rp *ApiKeyRP) Install(db *storage.MongoDB) error {
-    var index mgo.Index
-    var err error
+	var index mgo.Index
+	var err error
 
-    // key in api key must be unique
-    index = mgo.Index{
-        Key: []string{"key"},
-        Unique: true,
-        DropDups: false,
-        Background: true, // See notes.
-        Sparse: true,
-    }
-    err = db.Coll(rp.CollectionName()).EnsureIndex(index)
+	// key in api key must be unique
+	index = mgo.Index{
+		Key:        []string{"key"},
+		Unique:     true,
+		DropDups:   false,
+		Background: true, // See notes.
+		Sparse:     true,
+	}
+	err = db.Coll(rp.CollectionName()).EnsureIndex(index)
 
-    return err
+	return err
 }
-
--- resources/customer.go
+++ resources/customer.go
@@ -1,76 +1,76 @@
 package resources
 
 import (
-    "gopkg.in/mgo.v2"
-    "gopkg.in/mgo.v2/bson"
-    "github.com/supple/gorest/core"
-    "github.com/supple/gorest/storage"
-    "github.com/supple/gorest/model"
+	"github.com/supple/gorest/core"
+	"github.com/supple/gorest/model"
+	"github.com/supple/gorest/storage"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 const REPO_CUSTOMER = "crm"
 
 // ### -- Customer repo
 
-type CustomerRP struct{
-    gt *core.Gateway
-    cc *core.CustomerContext
+type CustomerRP struct {
+	gt *core.Gateway
+	cc *core.CustomerContext
 }
 
 func NewCustomerRP(cc *core.CustomerContext) *CustomerRP {
-    rp := &CustomerRP{cc:cc}
-    db := storage.GetInstance(REPO_CUSTOMER)
-    d := core.ContextDecorator(cc)
-    gt := core.NewGateway(rp.CollectionName(), d, db)
-    rp.gt = gt
+	rp := &CustomerRP{cc: cc}
+	db := storage.GetInstance(REPO_CUSTOMER)
+	d := core.ContextDecorator(cc)
+	gt := core.NewGateway(rp.CollectionName(), d, db)
+	rp.gt = gt
 
-    return rp
+	return rp
 }
 
 func (rp *CustomerRP) Create(model *model.Customer) error {
-    _, err := rp.FindOneByName(model.CustomerName)
-    if (err != nil) {
-        if (err != core.ErrNotFound) {
-            return err
-        }
-        //_, ok := err.(*core.ErrObjectNotFound)
-        //if (!ok) {
-        //    return err
-        //}
+	_, err := rp.FindOneByName(model.CustomerName)
+	if err != nil {
+		if err != core.ErrNotFound {
+			return err
+		}
+		//_, ok := err.(*core.ErrObjectNotFound)
+		//if (!ok) {
+		//    return err
+		//}
+		model.SetBasicFields()
+		model.Hash = core.RandString(8)
+		return rp.gt.Insert(model)
+	}
 
-        model.Hash = core.RandString(8)
-        return rp.gt.Insert(model)
-    }
-
-    return err
+	return err
 }
 
 func (rp *CustomerRP) Update(id string, model *map[string]interface{}) error {
-    //err := db.Coll(rp.CollectionName()).Update(bson.M{"_id": id}, model)
-    err := rp.gt.Update(id, model)
-    return err
+	//err := db.Coll(rp.CollectionName()).Update(bson.M{"_id": id}, model)
+	err := rp.gt.Update(id, model)
+	return err
 }
 
 func (rp *CustomerRP) FindOne(id string) (*model.Customer, error) {
 	result := &model.Customer{}
-    err := rp.gt.FindById(id, result)
+	err := rp.gt.FindById(id, result)
 	return result, err
 }
 
 func (rp *CustomerRP) FindOneByName(customerName string) (*model.Customer, error) {
 	result := &model.Customer{}
-    conditions := bson.M{CUSTOMER_NAME_FIELD: customerName}
+	conditions := bson.M{CUSTOMER_NAME_FIELD: customerName}
 	err := rp.gt.FindOneBy(conditions, result)
 	return result, err
 }
 
 func (rp *CustomerRP) FindOneBy(conditions bson.M) (*model.Customer, error) {
 	result := &model.Customer{}
-    err := rp.gt.FindOneBy(conditions, result)
+	err := rp.gt.FindOneBy(conditions, result)
 	return result, err
 }
 
-func (rp *CustomerRP) Delete(id string) (error) {
+func (rp *CustomerRP) Delete(id string) error {
 	err := rp.gt.Remove(id)
 	return err
 }
@@ -80,24 +80,24 @@ func (rp CustomerRP) CollectionName() string {
 }
 
 func CreateCustomer(name string) (*model.Customer, error) {
-    cc := &core.CustomerContext{CustomerName: name}
-    cRp := NewCustomerRP(cc)
-    c := &model.Customer{}
-    c.CustomerName = name
-    err := cRp.Create(c)
-    if (err != nil) {
-        return nil, err
-    }
-    return c, err
+	cc := &core.CustomerContext{CustomerName: name}
+	cRp := NewCustomerRP(cc)
+	c := &model.Customer{}
+	c.CustomerName = name
+	err := cRp.Create(c)
+	if err != nil {
+		return nil, err
+	}
+	return c, err
 }
 
 func (rp *CustomerRP) Install(db *storage.MongoDB) error {
 	index := mgo.Index{
-		Key: []string{CUSTOMER_NAME_FIELD},
-		Unique: true,
-		DropDups: false,
+		Key:        []string{CUSTOMER_NAME_FIELD},
+		Unique:     true,
+		DropDups:   false,
 		Background: true, // See notes.
-		Sparse: true,
+		Sparse:     true,
 	}
 	err := db.Coll(rp.CollectionName()).EnsureIndex(index)
 	return err
